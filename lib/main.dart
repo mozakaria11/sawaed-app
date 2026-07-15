4da79c51698ab03cc2a846d0d78937cc3f567ca0
mozakaria11 <mozakaria111@gmail.com>
2026-07-15 11:36:10 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'theme/app_theme.dart';
 import 'screens/login_screen.dart';
 
@@ -16,6 +17,11 @@ class SawaedApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: AppTheme.light(),
       locale: const Locale('ar'),
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
       supportedLocales: const [Locale('ar')],
       builder: (context, child) {
         return Directionality(
